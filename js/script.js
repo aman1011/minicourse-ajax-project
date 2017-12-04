1795ad72cd07aa422ef0bc608e8048251f143b7d
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-04 18:28:31 +0530

    Initial commit from udacity + google street image in script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var city = $('#city').val();
+
+    address = street + ',' + city;
+    $greeting.text('So, you want to live in ' + address + ' ?');
+
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '">');
+
+    return ;
+};
+
+$('#form-container').submit(loadData);
