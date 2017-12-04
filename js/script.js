a78195f57ecf53a9cf1823d46668ccd58bbc579c
Aman Gaur <aman.gaur1011@gmail.com>
2017-12-05 00:40:00 +0530

    Getting wikipedia Links

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function loadData() {
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '">');
 
     // Your NYTimes AJAX request.
-    request_url = "https://ai.nytimes.com/svc/search/v2/articlesearch.json?query=" + city + "&sort=newest&apikey=cd70092e341c42d1b617931f450d744d";
+    request_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?query=" + city + "&sort=newest&apikey=cd70092e341c42d1b617931f450d744d";
     $.getJSON(request_url, function(data) {
         $nytHeaderElem.text('New York Times Articles about '+ city);
         console.log('Reached here');
@@ -37,6 +37,24 @@ function loadData() {
     }).fail(function(e) {
         $nytHeaderElem.text('NY Times was not able to display the articles');
     });
+
+    // The wikipedia API url.
+    wiki_url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + city + "&format=json&callback=wikicallback";
+    $.ajax({
+        url: wiki_url, 
+        dataType:'jsonp',
+        success: function(response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+        }
+        //error: function(e) {
+    });
+        //}
 
     return false;
 };
